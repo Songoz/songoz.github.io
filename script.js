a268668817f8029fbebde39f04c11931994e7543
Songoz <songore01@gmail.com>
2026-08-03 21:10:21 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const slides = document.querySelectorAll(".work-slide");
+
+const next = document.querySelector(".next");
+
+const prev = document.querySelector(".prev");
+
+let current = 0;
+
+function showSlide(index){
+
+slides.forEach(slide => slide.classList.remove("active"));
+
+slides[index].classList.add("active");
+
+}
+
+next.addEventListener("click",()=>{
+
+current++;
+
+if(current>=slides.length){
+
+current=0;
+
+}
+
+showSlide(current);
+
+});
+
+prev.addEventListener("click",()=>{
+
+current--;
+
+if(current<0){
+
+current=slides.length-1;
+
+}
+
+showSlide(current);
+
+});
+
+showSlide(current);
